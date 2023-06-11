1c4b5d2dbed675593e2eb6ca6046cb2cd3204146
임은호 <>
2023-06-11 21:02:09 +0900

    로그인 확인 미들웨어에 응답형식 객체적용

--- middlewares/checkLogin.js
+++ middlewares/checkLogin.js
@@ -16,9 +16,8 @@ module.exports = async (req, res, next) => {
     // const Authorization = req.header("Authorization"); //토큰을 헤더로 받는 경우
     //토큰이 있는지 확인
     if (!Authorization) {
-      return res
-        .status(403)
-        .json({ errorMessage: "로그인이 필요한 서비스입니다." });
+      const response = new ApiResponse(403, "로그인이 필요한 서비스입니다.");
+      return res.status(403).json(response);
     }
 
     const [authType, authToken] = Authorization.split(" ");
@@ -26,19 +25,21 @@ module.exports = async (req, res, next) => {
 
     //authTyep === Bearer인지 확인
     if (authType !== "Bearer" || !authToken) {
-      return res.status(403).json({ errorMessage: "토큰 정보 오류" });
+      const response = new ApiResponse(403, "토큰 정보 오류");
+      return res.status(403).json(response);
     }
 
     //
     const { userId } = jwt.verify(authToken, "checkLogin_key");
     const user = await Users.findOne({ where: { userId } });
-    console.log(userId);
 
     res.locals.user = user;
     next();
   } catch (error) {
-    return res
-      .status(500)
-      .json({ errorMessage: "로그인이 필요한 서비스입니다." });
+    const response = new ApiResponse(
+      500,
+      "예상하지 못한 서버 문제가 발생했습니다."
+    );
+    return res.status(500).json(response);
   }
 };

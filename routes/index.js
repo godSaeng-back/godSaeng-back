7859245061e0a05212c46b213f1db3d013c73652
chang <https://github.com/BYUNCHANGILL>
2023-06-17 16:10:48 +0900

    라우터 설정 변경

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,9 @@
-// const express = require("express");
-// const app = express();
-
-// const feeds = require("./feeds");
-// const users = require("./users");
-
-// module.exports = [feeds, users];
-
 const express = require("express");
 const router = express.Router();
 
 const feedsRoutes = require("./feeds");
 const usersRoutes = require("./users");
 
-router.use('/feeds', feedsRoutes);
-router.use('/users', usersRoutes);
+router.use('/', [feedsRoutes, usersRoutes]);
 
 module.exports = router;

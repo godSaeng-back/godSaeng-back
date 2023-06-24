c1a7a286499cd601c99b161cfb903829c1526bdc
임은호 <>
2023-06-24 18:49:03 +0900

    배포 테스트

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const indexRouter = require('./routes/index.js');
 const app = express();
 const cors = require('cors');
 
+
+// EB 배포 위해 필요한 설정
+const createError = require('http-errors');
+const path = require('path');
+const logger = require('morgan');
+
+
 // CORS 설정
 app.use(
   cors({
@@ -19,18 +26,33 @@ app.use(
   })
 );
 
-// const indexRouter = require('./routes/index.js');
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use('/', [indexRouter]);
-// app.use('/', indexRouter);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.status(200).send('godSaeng diary backend API');
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.listen(3000, () => {
-  console.log('3000 포트로 서버 연결');
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,13 @@
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
 const { Users } = require("../models");

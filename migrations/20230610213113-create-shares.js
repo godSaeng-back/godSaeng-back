5e22334c0ed29e4fb32e8fbc0873068e2a74cde7
임은호 <>
2023-07-08 20:39:59 +0900

    배포 테스트2

---
+++ migrations/20230610213113-create-shares.js
@@ -0,0 +1,55 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Shares", {
+      shareId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      UserId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users", // Users 모델을 참조합니다.
+          key: "userId", // Users 모델의 userId를 참조합니다.
+        },
+        onDelete: "CASCADE", // 만약 Users 모델의 userId가 삭제되면, Posts 모델의 데이터가 삭제됩니다.
+      },
+      title: {
+        allowNull: false, // NOT NULL
+        type: Sequelize.STRING,
+      },
+      content: {
+        allowNull: false, // NOT NULL
+        type: Sequelize.STRING,
+      },
+      shareName: {
+        allowNull: false, // NOT NULL
+        type: Sequelize.STRING,
+      },
+      imagePath: {
+        allowNull: false, // NOT NULL
+        type: Sequelize.STRING,
+      },
+      anonymous: {
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Feeds");
+  },
+};
---
+++ models/shares.js
@@ -0,0 +1,71 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Shares extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.Users, {
+        // 2. Users 모델에게 N:1 관계 설정을 합니다.
+        targetKey: "userId", // 3. Users 모델의 userId 컬럼을
+        foreignKey: "UserId", // 4. Chats 모델의 UserId 컬럼과 연결합니다.
+      });
+    }
+  }
+  Shares.init(
+    {
+      shareId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      UserId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Users", // Users 모델을 참조합니다.
+          key: "userId", // Users 모델의 userId를 참조합니다.
+        },
+        onDelete: "CASCADE", // 만약 Users 모델의 userId가 삭제되면, Posts 모델의 데이터가 삭제됩니다.
+      },
+      title: {
+        allowNull: false, // NOT NULL
+        type: DataTypes.STRING,
+      },
+      content: {
+        allowNull: false, // NOT NULL
+        type: DataTypes.STRING,
+      },
+      shareName: {
+        allowNull: false, // NOT NULL
+        type: DataTypes.STRING,
+      },
+      imagePath: {
+        allowNull: false, // NOT NULL
+        type: DataTypes.STRING,
+      },
+      anonymous: {
+        type: DataTypes.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Shares",
+    }
+  );
+  return Shares;
+};
